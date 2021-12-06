167d0ae8bef6321368c60675cfbe0ffb6d7ccfc8
taro0 <yutaro.ono.418@gmail.com>
2021-12-07 00:30:57 +0900

    day 02, part 2

--- day02/main.ml
+++ day02/main.ml
@@ -9,9 +9,8 @@ let read_input () =
   with
     End_of_file -> (close_in ic; !acc)
 
-(* calc_position : string list -> int * int *)
-let calc_position input =
-  List.fold_right
+let print_part1 input =
+  let (horiz, depth) = List.fold_right
     (fun command (h, d) ->
       match String.split_on_char ' ' command with
           op :: x_str :: [] ->
@@ -22,11 +21,28 @@ let calc_position input =
             else assert false
         | _ -> assert false)
     input
-    (0, 0)
+    (0, 0) in
+  (print_string "part1: "; print_int (horiz * depth); print_newline())
+
+let print_part2 input =
+  let (horiz, depth, _) = List.fold_right
+    (fun command (h, d, a) ->
+      match String.split_on_char ' ' command with
+          op :: x_str :: [] ->
+            let x = int_of_string x_str in
+            if op = "forward" then (h + x, d + a * x, a)
+            else if op = "down" then (h, d, a + x)
+            else if op = "up" then (h, d, a - x)
+            else assert false
+        | _ -> assert false)
+    input
+    (* (horizontal, depth, aim) *)
+    (0, 0, 0) in
+  (print_string "part2: "; print_int (horiz * depth); print_newline())
 
 let main () =
   let input = read_input () in
-  let (horiz, depth) = calc_position input in
-  (print_int (horiz * depth); print_newline())
+  (print_part1 input;
+   print_part2 input)
 
 let _ = main ()
