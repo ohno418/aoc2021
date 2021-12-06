415f7ef51b454ee78bda3fe5aebcba3a8e29d8c5
taro0 <yutaro.ono.418@gmail.com>
2021-12-07 00:13:50 +0900

    day 02, part 1

---
+++ day02/main.ml
@@ -0,0 +1,28 @@
+(* read_input : unit -> stirng list *)
+let read_input () =
+  let ic = open_in "./input/day02.txt" in
+  let acc = ref [] in
+  try
+    (while true do
+       acc := (input_line ic) :: !acc
+    done; !acc)
+  with
+    End_of_file -> (close_in ic; !acc)
+
+let main () =
+  let input = read_input () in
+  let (horiz, depth) = List.fold_right
+    (fun command (h, d) ->
+      match String.split_on_char ' ' command with
+          op :: x_str :: [] ->
+            let x = int_of_string x_str in
+            if op = "forward" then (h + x, d)
+            else if op = "down" then (h, d + x)
+            else if op = "up" then (h, d - x)
+            else assert false
+        | _ -> assert false)
+    input
+    (0, 0) in
+  (print_int (horiz * depth); print_newline())
+
+let _ = main ()
